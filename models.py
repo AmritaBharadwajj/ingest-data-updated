f814af5126b46a4524e870aed298bd16d00af5b7
AmritaBharadwajj <140479183+AmritaBharadwajj@users.noreply.github.com>
2023-09-11 18:29:34 +0530

    Update models.py

--- models.py
+++ models.py
@@ -21,6 +21,9 @@ class Actor(Base):
     caller_type = Column(String)
     email = Column(String)
     profile_id = Column(String)
+    
+    # Define the one-to-many relationship with Activity
+    activities = relationship('Activity', back_populates='actor')
 
     def __init__(self, caller_type, email, profile_id):
         self.caller_type = caller_type
@@ -34,12 +37,14 @@ class Event(Base):
     type = Column(String)
     name = Column(String)
     activity_id = Column(Integer, ForeignKey('activities.id'))
-    parameters = Column(JSON)
+    #parameters = Column(JSON)
+    
+    # Define the one-to-one relationship with Activity
+    activity = relationship('Activity', back_populates='event')
 
-    def __init__(self, type, name, activity_id, parameters=None):
+    def __init__(self, type, name, parameters=None):
         self.type = type
         self.name = name
-        self.activity_id = activity_id
         self.parameters = parameters
 
 class Activity(Base):
@@ -53,8 +58,11 @@ class Activity(Base):
     actor_id = Column(Integer, ForeignKey('actors.id'))
     ip_address = Column(String)
 
+    # Define the many-to-one relationship with Actor
     actor = relationship('Actor', back_populates='activities')
-    events = relationship('Event', back_populates='activity')
+    
+    # Define the one-to-one relationship with Event
+    event = relationship('Event', uselist=False, back_populates='activity')
 
     def __init__(self, time, unique_qualifier, application_name, customer_id, actor_id, ip_address):
         self.time = time
@@ -63,5 +71,6 @@ class Activity(Base):
         self.customer_id = customer_id
         self.actor_id = actor_id
         self.ip_address = ip_address
+
 
 
